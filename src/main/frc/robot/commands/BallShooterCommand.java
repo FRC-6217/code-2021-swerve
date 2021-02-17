f0fc12d2aab0516cc5b1c2f1900c2616529029f5
CF-Safety-Captain <22ew01@cf.k12.mn.us>
2021-02-16 20:34:19 -0600

    java all red, frc and friends are green and yellow

---
+++ src/main/frc/robot/commands/BallShooterCommand.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ballShooter;
+
+public class BallShooterCommand extends CommandBase {
+  boolean ifOn;
+  ballShooter shooter;
+  /**
+   * Creates a new ShooterCommand.
+   */
+  public BallShooterCommand(ballShooter shooter, boolean ifOn) {
+    addRequirements(shooter);
+    this.shooter = shooter;
+    this.ifOn = ifOn;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    SmartDashboard.putNumber("Top Shoot Set", 2500);
+    SmartDashboard.putNumber("Bottom Shoot Set", 2500);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //TODO make distance based
+    double top = SmartDashboard.getNumber("Top Shoot Set", 0);
+    double bottom = SmartDashboard.getNumber("Bottom Shoot Set", 0);
+    shooter.onPID(top, bottom);
+    // shooter.on();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooter.off();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // if (!ifOn) {
+    //   return true;
+    // }
+    return false;
+  }
+}
