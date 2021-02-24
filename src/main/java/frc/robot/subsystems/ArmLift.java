833698c559de84b644e17cf7b17e78b685b2c2be
CF-Safety-Captain <22ew01@cf.k12.mn.us>
2021-02-23 20:23:04 -0600

    cleaning up the armlift branch

--- src/main/java/frc/robot/subsystems/ArmLift.java
+++ src/main/java/frc/robot/subsystems/ArmLift.java
@@ -1,9 +1,6 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
@@ -13,105 +10,116 @@ import com.revrobotics.CANDigitalInput.LimitSwitchPolarity;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ARM_LIFT_CONSTANTS;
 
+
+
 public class ArmLift extends SubsystemBase {
+  // varibles for motors
+  private CANSparkMax left;
+  private CANSparkMax right;
 
-  private CANSparkMax leftMotor;
-  private CANSparkMax rightMotor;
-  private CANDigitalInput leftLimitUp;
-  private CANDigitalInput leftLimitDown;
-  private CANDigitalInput rightLimitUp;
-  private CANDigitalInput rightLimitDown;
-  private int leftDirection = 1;
-  private int rightDirection = 1;
-  
+  // varibles for limit switches
+  private CANDigitalInput upLeftLimit;
+  private CANDigitalInput upRightLimit;
+  private CANDigitalInput downLeftLimit;
+  private CANDigitalInput downRightLimit;
+
+
+  /** Creates a new ArmLift. */
   public ArmLift() {
-    leftMotor = new CANSparkMax(ARM_LIFT_CONSTANTS.MOTOR_CONTROLLER_ID_LEFT, MotorType.kBrushed);
-    rightMotor = new CANSparkMax(ARM_LIFT_CONSTANTS.MOTOR_CONTROLLER_ID_RIGHT, MotorType.kBrushed);
+    // creates the motor controller objects
+    left = new CANSparkMax(ARM_LIFT_CONSTANTS.MOTOR_CONTROLLER_ID_LEFT, MotorType.kBrushed);
+    right = new CANSparkMax(ARM_LIFT_CONSTANTS.MOTOR_CONTROLLER_ID_RIGHT, MotorType.kBrushed);
 
-    leftMotor.restoreFactoryDefaults();
-    rightMotor.restoreFactoryDefaults();
+    // creates the limit switch objects
+    upLeftLimit = left.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
+    upRightLimit = right.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
+    downLeftLimit = left.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
+    downRightLimit = right.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
 
-    leftMotor.setIdleMode(IdleMode.kBrake);
-    rightMotor.setIdleMode(IdleMode.kBrake);
-    
-    leftLimitUp = leftMotor.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
-    leftLimitDown = leftMotor.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
+    // restores factory defult
+    left.restoreFactoryDefaults();
+    right.restoreFactoryDefaults();
 
-    rightLimitUp = rightMotor.getForwardLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
-    rightLimitDown = rightMotor.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
-    
-    if (ARM_LIFT_CONSTANTS.IS_NEGATED_LEFT) {
-      leftDirection = -1;
+    // tells the wheels to stop 
+    left.setIdleMode(IdleMode.kBrake);
+    right.setIdleMode(IdleMode.kBrake);
+
+    // set motors inverted when negated
+    if(ARM_LIFT_CONSTANTS.IS_NEGATED_LEFT) {
+      left.setInverted(true);
     }
-    if (ARM_LIFT_CONSTANTS.IS_NEGATED_RIGHT) {
-      rightDirection = -1;
-    }
-  }
-
-  public void up(){
-    leftMotor.set(leftDirection * ARM_LIFT_CONSTANTS.SPEED);
-    rightMotor.set(rightDirection * ARM_LIFT_CONSTANTS.SPEED);
-  }
-
-  public void down(){
-    leftMotor.set(-leftDirection * ARM_LIFT_CONSTANTS.SPEED);
-    rightMotor.set(-rightDirection * ARM_LIFT_CONSTANTS.SPEED);
-  }
-
-  public void upLimit() {
-    if(!leftLimitUp.get()) {
-      leftMotor.set(leftDirection * ARM_LIFT_CONSTANTS.SPEED);
-    }
-    else{
-      leftMotor.set(0);
-    }
-
-    if(!rightLimitUp.get()){
-      rightMotor.set(rightDirection * ARM_LIFT_CONSTANTS.SPEED);
-    }
-    else{
-      rightMotor.set(0);
+    if(ARM_LIFT_CONSTANTS.IS_NEGATED_RIGHT) {
+      right.setInverted(true);
     }
   }
 
-  public void downLimit() {
-    if(!leftLimitDown.get()) {
-      leftMotor.set(-leftDirection * ARM_LIFT_CONSTANTS.SPEED);
-    }
-    else{
-      leftMotor.set(0);
-    }
-
-    if(!rightLimitDown.get()){
-      rightMotor.set(-rightDirection * ARM_LIFT_CONSTANTS.SPEED);
-    }
-    else{
-      rightMotor.set(0);
-    }
+  //sensor methods
+  public boolean getUpLeftLimit() {
+    return upLeftLimit.get();
   }
 
-  public void off() {
-      leftMotor.set(0);
-      rightMotor.set(0);
+  public boolean getUpRightLimit() {
+    return upRightLimit.get();
   }
 
-  public boolean getUpperLimits(){
-    return(leftLimitUp.get() && rightLimitUp.get());
+  public boolean getDownLeftLimit() {
+    return downLeftLimit.get();
   }
 
-  public boolean getLowerLimits(){
-    return(leftLimitDown.get() && rightLimitDown.get());
+  public boolean getDownRightLimit() {
+    return downRightLimit.get();
+  }
+
+  // motor control methods
+
+  // up motors
+  public void upLeftMotor() {
+    left.set(ARM_LIFT_CONSTANTS.SPEED);
+  }
+
+  public void upRightMotor() {
+    right.set(ARM_LIFT_CONSTANTS.SPEED);
+  }
+
+  // down motors
+  public void downLeftMotor() {
+    left.set(-ARM_LIFT_CONSTANTS.SPEED);
+  }
+
+  public void downRightMotor() {
+    right.set(-ARM_LIFT_CONSTANTS.SPEED);
+  }
+
+  // off motors
+  public void offLeftMotor() {
+    left.set(0);
   }
   
+  public void offRightMotor() {
+    right.set(0);
+  }
+  
+  // calling previous functions
+  public void upMotor() {
+    upLeftMotor();
+    upRightMotor();
+  }
+
+  public void downMotor() {
+    downLeftMotor();
+    downRightMotor();
+  }
+  
+  public void offMotor() {
+    offLeftMotor();
+    offRightMotor();
+  }
+
   @Override
   public void periodic() {
-    SmartDashboard.putBoolean("Left Limit UP", leftLimitUp.get());
-    SmartDashboard.putBoolean("Left Limit DOWN", leftLimitDown.get());
-    SmartDashboard.putBoolean("Right Limit UP", rightLimitUp.get());
-    SmartDashboard.putBoolean("Right Limit DOWN", rightLimitDown.get());
+    // This method will be called once per scheduler run
   }
 }
